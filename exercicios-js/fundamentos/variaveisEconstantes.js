5fcb5809f292739f419c0be8b8914de6e84d158c
Renan Luz <ren4n@live.com>
2020-01-13 08:46:05 -0300

    Variáveis e constantes em javascript

---
+++ exercicios-js/fundamentos/variaveisEconstantes.js
@@ -0,0 +1,19 @@
+var a = 3
+let b = 4
+
+// Variáveis do tipo var, podem ser redeclaradas dentro do mesmo escopo
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+// Constantes não podem ter seus valores reatribuído 
+const c = 5
+// c = 50 
+
+console.log(c)
