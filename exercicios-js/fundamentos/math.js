e308df0bf77e9c70b9ca7fc6d57061ecf0eb3c6d
Renan Luz <ren4n@live.com>
2020-01-13 09:54:44 -0300

    Objeto Math

---
+++ exercicios-js/fundamentos/math.js
@@ -0,0 +1,11 @@
+const raio = 5.6
+
+/*
+   Math é um objeto que possue uma constante chamada PI
+   e uma função que calcula a potência pow
+*/ 
+const area = Math.PI * Math.pow(raio, 2)
+
+
+console.log(area)
+console.log(typeof Math)
