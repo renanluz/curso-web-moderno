3b3596d295ad50c24edfd815b70c474d9ec633fa
Renan Luz <ren4n@live.com>
2020-01-13 09:47:42 -0300

    Alguns cuidados com números em javascript

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+// Resulta em um número do tipo infinity
+console.log(7 / 0) 
+
+// Será verificado a possibílidade de conversão da String para number, e automáticamente já é feita a divisão
+console.log ("10" / 2 )
+console.log ("Show" * 2)
+
+// Ocorrerá algumas imprecisões em operações, devido o javascript utilizar o padrão IEEE 754
+console.log (0.1 + 0.7)
