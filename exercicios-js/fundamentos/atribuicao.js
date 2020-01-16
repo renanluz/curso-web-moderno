813c857101bc2832af88fe45e52bb8e3513c286c
Renan Luz <ren4n@live.com>
2020-01-16 11:00:07 -0300

    Operadores de atribuição

---
+++ exercicios-js/fundamentos/atribuicao.js
@@ -0,0 +1,23 @@
+const a = 7
+
+let b = 3
+
+// Atribuição aditiva: b = b + a ou b = 7 +3
+b += a 
+console.log(b)
+
+// Atribuição subtrativa: b = b - 4 ou b = 10 - 4
+b -= 4
+console.log(b)
+
+// Atribuição multiplicativa: b = b * 2 ou b = 6 * 2
+b *= 2 
+console.log(b)
+
+// Atribuição divisiva: b = b / 2 ou b = 12 / 2
+b /= 2
+console.log(b)
+
+// Atribuição modular: b = b % 2 ou b = 6 % 2
+b %= 2
+console.log(b)
