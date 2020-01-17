eed32950f61f73a3e473bdb5899eb4b914d6d451
Renan Luz <ren4n@live.com>
2020-01-17 14:57:31 -0300

    Operadores ternários

---
+++ exercicios-js/fundamentos/ternario.js
@@ -0,0 +1,10 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+
+/**
+ * Estrutura do operador ternário
+ * condição? valor se for verdareiro : valor se for falso
+ */ 
